6e45f87da8ba41eb9f900c5388d8e5d87e2afb71
daniel anthony <s2500142@BC-C02J64AGQ6LRs-MacBook-Air.local>
2025-01-16 20:20:24 +0200

    Adding data type exercise

---
+++ week_01/exercise_01/data_types/data_types.js
@@ -0,0 +1,23 @@
+// 1. create a variable called `age` and assign it a number (e.g. 25).
+let age = 25;
+
+// 2. create a variable called `name` and assign it a string (e.g. "John").
+const name = "John";
+
+// 3. create a variable called `isStudent` and assign it a boolean (e.g. true or false).
+const isStudent = true;
+
+// 4. create a variable called `hobbies` and assign it an array with few strings strings.
+let hobbies = ["reading", "swimming", "cooking"];
+
+// 5. create an object called `info` with two properties: schoolName (a string, e.g.,"Business College Helsinki") and groupName (a number, e.g. REACT25K).
+let info = {
+  schoolName: "Business College Helsinki",
+  groupName: "REACT25K",
+};
+// use console.log() to print out each variables.
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(hobbies);
+console.log(info);
